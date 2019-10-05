9a1a2e712007bf657580297e4b4eafaa7fadeaf1
Kristine Glazuna <kglazuna@twilio.com>
2019-10-05 19:41:29 +0300

    Use Timber logger

---
+++ app/src/main/java/com/kglazuna/app/CatApplication.kt
@@ -0,0 +1,15 @@
+package com.kglazuna.app
+
+import android.app.Application
+import timber.log.Timber
+
+class CatApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
