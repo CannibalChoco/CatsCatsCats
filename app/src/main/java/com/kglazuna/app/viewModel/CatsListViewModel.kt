d03007b5d6a46eba0605621abccdc5e22ffcbe09
Kristine Glazuna <kglazuna@twilio.com>
2019-10-06 20:23:12 +0300

    Don't send new request on screen rotation

--- app/src/main/java/com/kglazuna/app/viewModel/CatsListViewModel.kt
+++ app/src/main/java/com/kglazuna/app/viewModel/CatsListViewModel.kt
@@ -18,9 +18,11 @@ class CatsListViewModel : ViewModel() {
     }
 
     fun getCats() {
-        viewModelScope.launch {
-            catList.value = CatRepo.getCats()
-            Timber.d("viewModel got cats")
+        if (catList.value.isNullOrEmpty()) {
+            viewModelScope.launch {
+                catList.value = CatRepo.getCats()
+                Timber.d("viewModel got cats")
+            }
         }
     }
 }
