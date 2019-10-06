defefc85a1f87d7a7fa7d687b2d44e944525573a
Kristine Glazuna <kglazuna@twilio.com>
2019-10-06 20:16:09 +0300

    Try to send vote only when there is network connection

--- app/src/main/java/com/kglazuna/app/ui/CatRatingActivity.kt
+++ app/src/main/java/com/kglazuna/app/ui/CatRatingActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.viewpager.widget.ViewPager
+import com.kglazuna.app.ConnectionStateMonitor
 import com.kglazuna.app.R
 import com.kglazuna.app.model.Vote
 import com.kglazuna.app.ui.adapter.CatImagePagerAdapter
@@ -20,6 +21,7 @@ class CatRatingActivity : AppCompatActivity() {
     private lateinit var viewModel: CatRatingViewModel
     private lateinit var viewPager: ViewPager
     private lateinit var pagerAdapter: CatImagePagerAdapter
+    private var isConnected = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,17 +38,44 @@ class CatRatingActivity : AppCompatActivity() {
         viewPager.currentItem = catPosition
 
         downVote.setOnClickListener {
-            viewModel.sendVote(viewPager.currentItem, Vote.Value.VOTE_DOWN.value)
+            tryVote(Vote.Value.VOTE_DOWN.value)
         }
 
         upVote.setOnClickListener {
-            viewModel.sendVote(viewPager.currentItem, Vote.Value.VOTE_UP.value)
+            tryVote(Vote.Value.VOTE_UP.value)
         }
 
         viewModel.onVotePosted.observe(this, Observer {
             displayVotedMessage(it)
         })
 
+        ConnectionStateMonitor.INSTANCE.isConnected.observe(this, Observer {
+            when (it) {
+                true -> Timber.d("Connected")
+                else -> Timber.d("Lost connectivity")
+            }
+
+            isConnected = it
+        })
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ConnectionStateMonitor.INSTANCE.register(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        ConnectionStateMonitor.INSTANCE.unregister(this)
+    }
+
+    private fun tryVote(vote: Int) {
+        if (isConnected) {
+            viewModel.sendVote(viewPager.currentItem, vote)
+        } else {
+            showSnackbar(catRatingLayout, getString(R.string.not_connected_cant_send_vote))
+        }
     }
 
     fun displayVotedMessage(votePosted: Boolean) = when(votePosted) {
